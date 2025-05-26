f4be254d60bb5f83c9e6e960823b5f424c919f39
thesarfo <ernestsarfo100@gmail.com>
2025-05-26 13:53:01 +0000

    feat: implement attribute for primary keys

---
+++ Dozer.Core/Attributes/KeyAttribute.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Dozer.Core.Attributes;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class KeyAttribute : Attribute
+{
+    public bool IsAutoIncrement { get; }
+
+    public KeyAttribute(bool isAutoIncrement = true)
+    {
+        IsAutoIncrement = isAutoIncrement;
+    }
+} 
